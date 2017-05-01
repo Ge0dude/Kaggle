7157f3ab6ebf23128a557d4a4129247da3514472
Brendon <btucker1992@gmail.com>
2017-05-02 07:52:51 +0800

    working through the Kaggle

--- Sherbank/SherbankExploration.py
+++ Sherbank/SherbankExploration.py
@@ -2,6 +2,18 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+
+taken from https://www.kaggle.com/sudalairajkumar/sberbank-russian-housing-
+market/simple-exploration-notebook-sberbank
 """
 
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+#from sklearn import model_selection, preprocessing
+#import xgboost as xgb
+color = sns.color_palette()
+
+train_df = pd.read_csv('/Users/brendontucker/KaggleData/SherbankData/SherbankTrain.csv')
+ 
