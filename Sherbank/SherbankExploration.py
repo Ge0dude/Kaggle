9fed4f5ec8a737912956aa8ec8e6ffc8f981915b
Brendon <btucker1992@gmail.com>
2017-05-03 15:43:47 +0800

    package management skills are lacking...

--- Sherbank/SherbankExploration.py
+++ Sherbank/SherbankExploration.py
@@ -11,8 +11,8 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import seaborn as sns
-#from sklearn import model_selection, preprocessing
-#import xgboost as xgb
+from sklearn import model_selection, preprocessing
+import xgboost as xgb
 color = sns.color_palette()
 
 train_df = pd.read_csv('/Users/brendontucker/KaggleData/SherbankData/SherbankTrain.csv')
