6bef7841b8e1335c42f82dc6b7ff344635507112
Brendon <btucker1992@gmail.com>
2017-05-03 16:20:47 +0800

    working on data exploration skills

--- Sherbank/SherbankExploration.py
+++ Sherbank/SherbankExploration.py
@@ -17,20 +17,73 @@ color = sns.color_palette()
 
 train_df = pd.read_csv('/Users/brendontucker/KaggleData/SherbankData/SherbankTrain.csv')
 
-plt.scatter(range(train_df.shape[0]), np.sort(train_df.price_doc.values)) 
+#plt.scatter(range(train_df.shape[0]), np.sort(train_df.price_doc.values)) 
 
 '''need to understand how scatter is working better'''
 #x = 4
 #y = [10, 11, 12, 13]
 #plt.scatter(range(x), y)
 #okay, much clearer now 
-sns.distplot(train_df.price_doc.values, kde=True)
-sns.distplot(np.log(train_df.price_doc.values), kde=True, bins=50)
+#sns.distplot(train_df.price_doc.values, kde=True)
+#sns.distplot(np.log(train_df.price_doc.values), kde=True, bins=50)
 
 #sns.distplot(np.log(train_df.full_sq.values), kde=True)
 
-train_df['yearmonth'] = train_df['timestamp'].apply(lambda x: x[:4]+x[5:7])
+#train_df['yearmonth'] = train_df['timestamp'].apply(lambda x: x[:4]+x[5:7])
 
-#need to understand this better
-Ltest = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-Ltest.apply(lambda x: x[:4]+x[5:7])
+for f in train_df.columns:
+    if train_df[f].dtype=='object':
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(list(train_df[f].values)) 
+        train_df[f] = lbl.transform(list(train_df[f].values))
+        
+train_y = train_df.price_doc.values
+train_X = train_df.drop(["id", "timestamp", "price_doc"], axis=1)
+
+xgb_params = {
+    'eta': 0.05,
+    'max_depth': 8,
+    'subsample': 0.7,
+    'colsample_bytree': 0.7,
+    'objective': 'reg:linear',
+    'eval_metric': 'rmse',
+    'silent': 1
+}
+
+
+
+''' #should take approx. one minute to run
+dtrain = xgb.DMatrix(train_X, train_y, feature_names=train_X.columns.values)
+
+model = xgb.train(dict(xgb_params, silent=0), dtrain, num_boost_round=100)
+
+xgb.plot_importance(model, max_num_features=100, height=0.8) #ax=ax)
+'''
+
+
+ulimit = np.percentile(train_df.price_doc.values, 99.5)
+llimit = np.percentile(train_df.price_doc.values, 0.5)
+train_df['price_doc'].ix[train_df['price_doc']>ulimit] = ulimit
+train_df['price_doc'].ix[train_df['price_doc']<llimit] = llimit
+
+col = "full_sq"
+ulimit = np.percentile(train_df[col].values, 99.5)
+llimit = np.percentile(train_df[col].values, 0.5)
+train_df[col].ix[train_df[col]>ulimit] = ulimit
+train_df[col].ix[train_df[col]<llimit] = llimit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
