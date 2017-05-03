a38eeaa2fc650a25c8eb0a98ac4cbfcd5b9a8758
Brendon <btucker1992@gmail.com>
2017-05-03 17:37:16 +0800

    might've found a good feature to split 'life_sq'--16% have a value of zero.

--- Sherbank/SherbankExploration.py
+++ Sherbank/SherbankExploration.py
@@ -30,13 +30,13 @@ train_df = pd.read_csv('/Users/brendontucker/KaggleData/SherbankData/SherbankTra
 #sns.distplot(np.log(train_df.full_sq.values), kde=True)
 
 #train_df['yearmonth'] = train_df['timestamp'].apply(lambda x: x[:4]+x[5:7])
-
-for f in train_df.columns:
-    if train_df[f].dtype=='object':
-        lbl = preprocessing.LabelEncoder()
-        lbl.fit(list(train_df[f].values)) 
-        train_df[f] = lbl.transform(list(train_df[f].values))
-        
+#
+#for f in train_df.columns:
+#    if train_df[f].dtype=='object':
+#        lbl = preprocessing.LabelEncoder()
+#        lbl.fit(list(train_df[f].values)) 
+#        train_df[f] = lbl.transform(list(train_df[f].values))
+'''        
 train_y = train_df.price_doc.values
 train_X = train_df.drop(["id", "timestamp", "price_doc"], axis=1)
 
@@ -50,6 +50,7 @@ xgb_params = {
     'silent': 1
 }
 
+'''
 
 
 ''' #should take approx. one minute to run
@@ -61,10 +62,12 @@ xgb.plot_importance(model, max_num_features=100, height=0.8) #ax=ax)
 '''
 
 
-ulimit = np.percentile(train_df.price_doc.values, 99.5)
+ulimit = np.percentile(train_df.price_doc.values, 99.5) #is this 3 sigma?
 llimit = np.percentile(train_df.price_doc.values, 0.5)
 train_df['price_doc'].ix[train_df['price_doc']>ulimit] = ulimit
 train_df['price_doc'].ix[train_df['price_doc']<llimit] = llimit
+
+
 
 col = "full_sq"
 ulimit = np.percentile(train_df[col].values, 99.5)
@@ -72,7 +75,31 @@ llimit = np.percentile(train_df[col].values, 0.5)
 train_df[col].ix[train_df[col]>ulimit] = ulimit
 train_df[col].ix[train_df[col]<llimit] = llimit
 
+sns.jointplot(x=np.log1p(train_df.full_sq.values), 
+              y=np.log1p(train_df.price_doc.values), kind="hex")
+plt.ylabel('Log of Price', fontsize=12)
+plt.xlabel('Log of Total area in square metre', fontsize=12)
 
+#dig into a lowervariable, look for splits, split, retry xgboost
+
+col = "life_sq"
+train_df[col].fillna(0, inplace=True)
+ulimit = np.percentile(train_df[col].values, 95)
+llimit = np.percentile(train_df[col].values, 5)
+train_df[col].ix[train_df[col]>ulimit] = ulimit
+train_df[col].ix[train_df[col]<llimit] = llimit
+
+sns.jointplot(x=np.log1p(train_df.life_sq.values), 
+              y=np.log1p(train_df.price_doc.values), 
+              kind='scatter', size=10)
+plt.ylabel('Log of Price', fontsize=12)
+plt.xlabel('Log of living area in square metre', fontsize=12)
+
+testCol = train_df['life_sq']
+
+sns.distplot(testCol, kde=True) #this shows something like 16% have a value of 
+#0. So I think it would be good to seperate that data into two groups, 0 and 
+#then the rest
 
 
 
