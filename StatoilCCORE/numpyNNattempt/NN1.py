5385edd9505191eab17f65998bae177a6ec0d271
Brendon <btucker1992@gmail.com>
2017-12-26 10:33:29 -0600

    it works! really poorly... but it works

--- StatoilCCORE/numpyNNattempt/NN1.py
+++ StatoilCCORE/numpyNNattempt/NN1.py
@@ -8,10 +8,18 @@ Created on Sun Dec 24 06:15:30 2017
 #%% IMPORTS
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #%% LOAD TRAIN
-train = pd.read_json("/Users/brendontucker/KaggleData/StatoilCCORE/data-1/processed/train.json")
+orginal_train = pd.read_json("/Users/brendontucker/KaggleData/StatoilCCORE/data-1/processed/train.json")
+
+#%% TEST TRAIN SPLIT
+msk = np.random.rand(len(orginal_train)) < 0.8
+train = orginal_train[msk]
+test = orginal_train[~msk]
+
 #%% LOAD TEST FILE 
+#resutl of this is what I submit
 test = pd.read_json("/Users/brendontucker/KaggleData/StatoilCCORE/data/processed/test.json")
 # wow, a 1.5 gb file loaded.... 
 
@@ -106,7 +114,7 @@ dtype: float64
 #%% CREATING DF OF TARGET VARIABLE
 #need to take each list of train.iloc[x] of 5625 float, and add as col
 #or row? Add as a row I believe. 
-for x in range(len(train)):
+#for x in range(len(train)):
     
 
 #%% CAN WE ADD SERIES TO CREATE A DF? 
@@ -116,5 +124,345 @@ addSeries = pd.Series(train.iloc[1][0])
     
     
     
-#%% TEST WITH HH ONLY 
+#%% NN SET UP 
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = 1/(1 + np.exp(-z))
+    ### END CODE HERE ###
+    
+    return s
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+    
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+    
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+    
+    ### START CODE HERE ### (≈ 1 line of code)
+    w = np.zeros((dim, 1))
+    b = 0
+    ### END CODE HERE ###
+
+    assert(w.shape == (dim, 1))
+    assert(isinstance(b, float) or isinstance(b, int))
+    
+    return w, b
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+    
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+    
+    m = X.shape[1]
+    
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = sigmoid(np.dot(w.T, X) + b)#sigmoid(w.T*X +b) -> led to a broadcast error   # compute activation
+    #print('test: A is;', A)
+    cost = (-1/m)*np.sum(Y*np.log(A) + (1-Y)*np.log(1-A)) #Y*np.log(A) + (1-Y)*np.log(1-A)
+    #print('test: cost is;', cost)
+    ### END CODE HERE ###
+    
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    dZ = A - Y #i added this to match format in videos
+    #print('test: dZ is;', dZ)
+    dw = (1/m)*np.dot(X, dZ.T) #(1/m)*X*dZ.T
+    db = (1/m)*np.sum(dZ)
+    ### END CODE HERE ###
+
+    assert(dw.shape == w.shape)
+    assert(db.dtype == float)
+    cost = np.squeeze(cost)
+    assert(cost.shape == ())
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+    
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+    
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+    
+    costs = []
+    
+    for i in range(num_iterations):
+        
+        
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ###  
+        grads, cost = propagate(w, b, X, Y)
+        ### END CODE HERE ###
+        
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w = w - learning_rate*dw
+        b = b - learning_rate*db
+        ### END CODE HERE ###
+        
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+        
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    ### START CODE HERE ### (≈ 1 line of code)
+    A = sigmoid(np.dot(w.T, X))  #[x = 0 if x<=.5 else x = 1 in A] 
+    #print(A, A[0], A.shape[1], A[0][0], 'Y_pred:', Y_prediction)
+    ### END CODE HERE ###
+    
+    #Y_predictionTest[0] = [x*0 if x<=.5 else math.ceil(x) for x in A[0]] #this is how to do it without a for loop?
+    
+    for i in range(A.shape[1]):
+    
+    # Convert probabilities A[0,i] to actual predictions p[0,i]
+        ### START CODE HERE ### (≈ 4 lines of code)
+        if A[0][i] <= .5:
+            Y_prediction[0][i] = 0
+        else:
+            Y_prediction[0][i] = 1
+        ### END CODE HERE ###
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
+
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    """
+    Builds the logistic regression model by calling the function you've implemented previously
+    
+    Arguments:
+    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
+    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
+    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
+    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
+    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
+    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+    print_cost -- Set to true to print the cost every 100 iterations
+    
+    Returns:
+    d -- dictionary containing information about the model.
+    """
+    
+    ### START CODE HERE ###
+    # initialize parameters with zeros (≈ 1 line of code)
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    ### END CODE HERE ###
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+
+#%% X TARGET VARIABLE SET UP
+    
+XtargetTrain = np.zeros(shape=(len(train),5625))
+for x in range(len(train)):
+    XtargetTrain[x] = train.iloc[x][0]
+XtargetTrain = XtargetTrain.T
+
+#%% WHAT WE HAVE NOW
+
+'''
+XtargetTrain.shape
+Out[64]: (5625, 1291)
+'''
+
+#%% Y TARGET VARIABLE SET UP
+
+YtargetTrain = np.zeros(shape=(len(train),1))
+for x in range(len(train)):
+    YtargetTrain[x] = train.iloc[x][4]
+YtargetTrain = YtargetTrain.T
+
+
+#%% X TEST VAR SET UP
+XtargetTest = np.zeros(shape=(len(test),5625))
+for x in range(len(test)):
+    XtargetTest[x] = test.iloc[x][0]
+XtargetTest = XtargetTest.T
+
+#%% Y TEST TARGET VARIABLE SET UP
+
+YtargetTest = np.zeros(shape=(len(test),1))
+for x in range(len(test)):
+    YtargetTest[x] = test.iloc[x][4]
+YtargetTest = YtargetTest.T
+
+#%% BASIC PREPROCESSING VARS
+
+mean = XtargetTrain.mean(axis=0)
+std = XtargetTrain.std(axis=0)
+std.shape
+
+#%% SUPER BASIC PREPROCESSING
+
+XtargetTrain = XtargetTrain/mean
+XtargetTrain = XtargetTrain - std
+
+
+# %% TESTSET PREPROCESSING 
+
+mean1 = XtargetTest.mean(axis=0)
+std1 = XtargetTest.std(axis=0)
+XtargetTest = XtargetTest/mean1
+XtargetTest = XtargetTest - std1
+
+#%% RUN THE NN
+'''
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, 
+          num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+'''
+d = model(XtargetTrain, YtargetTrain, XtargetTest, YtargetTest, 
+          num_iterations = 100000, learning_rate = 0.0001, print_cost = True)
+
+
+#%% playing with hyperparameters before moving on to a more complex model
+'''
+d = model(XtargetTrain, YtargetTrain, XtargetTest, YtargetTest, 
+          num_iterations = 10000, learning_rate = 0.0001, print_cost = True)
+
+Cost after iteration 9900: 0.608378
+train accuracy: 66.69248644461658 %
+test accuracy: 59.42492012779553 %
+
+d = model(XtargetTrain, YtargetTrain, XtargetTest, YtargetTest, 
+          num_iterations = 10000, learning_rate = 0.0005, print_cost = True)
+
+Cost after iteration 9900: 0.674620
+train accuracy: 64.44616576297443 %
+test accuracy: 57.18849840255591 %
+
+d = model(XtargetTrain, YtargetTrain, XtargetTest, YtargetTest, 
+          num_iterations = 20000, learning_rate = 0.0001, print_cost = True)
+
+Cost after iteration 19900: 0.572093
+train accuracy: 71.3400464756003 %
+test accuracy: 65.814696485623 %
+
+d = model(XtargetTrain, YtargetTrain, XtargetTest, YtargetTest, 
+          num_iterations = 40000, learning_rate = 0.0001, print_cost = True)
+
+Cost after iteration 39900: 0.529754
+train accuracy: 75.6003098373354 %
+test accuracy: 68.05111821086263 %
+
+d = model(XtargetTrain, YtargetTrain, XtargetTest, YtargetTest, 
+          num_iterations = 100000, learning_rate = 0.0001, print_cost = True)
+
+Cost after iteration 99900: 0.458904
+train accuracy: 82.21191028615623 %
+test accuracy: 65.27331189710611 %
+
+'''
+
+#%% appears that our train accuracy is improving faster than the test accuracy
+#would like to get > 90% train accuracy before making any judgements  
+
 
